f100875a09c05c18b0e04ad3e3df58506f22e77a
Jonax79409 <gourav228@gmail.com>
2020-08-31 14:27:35 +0530

    Completed server routes

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose');
 const bcrypt = require ('bcrypt');
 const jwt = require ('jsonwebtoken');
-const config = require('../config/config.js');
+const config = require('../config/config').get(process.env.NODE_ENV);
 const SALT_I = 10;
 
 const userSchema = mongoose.Schema ({
@@ -32,6 +32,89 @@ const userSchema = mongoose.Schema ({
         type: String
     }
 })
+
+//Why ES6 not working here ????? -_-  :3 :3
+// userSchema.pre ('save', (next) => {
+//     var user = this;
+
+//     if (user.isModified('password')) {
+//         bcrypt.genSalt(SALT_I, (err,salt) => {
+//             if (err) {
+//                 return next(err)
+//             }
+//             bcrypt.hash (user.password, salt, (err,hash) => {
+//                 if (err) return next(err)
+
+//                 user.password = hash;
+//                 next();
+//             })
+//         })
+//     }
+//     else {
+//         next();
+//     }
+// })
+
+userSchema.pre('save',function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, function (err,salt) {
+            if (err) return next(err)
+    
+            bcrypt.hash(user.password, salt,function (err,hash) {
+                if (err) return next(err);
+    
+                user.password = hash;
+                next ();
+            })
+        })
+    } else {
+        next();
+    }
+    
+})
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+    var user = this;
+    bcrypt.compare (candidatePassword, user.password, function (err, isMatch) {
+        if (err) return cb(err)
+        cb (null, isMatch)
+    })
+}
+
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),config.SECRET);
+
+    user.token = token;
+    user.save(function(err,user){
+        if(err) return cb(err);
+        cb(null,user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+
+    jwt.verify(token, config.SECRET,function (err, decode) {
+
+        user.findOne({"_id":decode, "token": token}, function(err,user) {
+            if (err) return cb(err)
+            cb(null,user)
+        })
+    })
+}
+
+userSchema.methods.deleteToken = function (token,cb) {
+    var user = this;
+
+    user.update({$unset:{token:1}}, (err,user) => {
+        if (err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
--- server/server.js
+++ server/server.js
@@ -3,18 +3,38 @@ const bodyParser = require ('body-parser');
 const cookiesParser = require ('cookie-parser');
 const mongoose = require ('mongoose');
 const app = express();
-const config = require('./config/config.js').get(process.env.NODE_ENV);
+const config = require('./config/config').get(process.env.NODE_ENV);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const { User } = require ('./models/user.js');
-const { Book } = require ('./models/book.js')
+const { Book } = require ('./models/book.js');
+const { auth } = require ('./middleware/auth.js')
 
 app.use(bodyParser.json());
-app.use(cookiesParser())
+app.use(cookiesParser());
+
 
 //GET
+
+app.get('/api/auth',auth,(req,res)=>{
+    res.json({
+        isAuth:true,
+        id:req.user._id,
+        email:req.user.email,
+        name:req.user.name,
+        lastname:req.user.lastname
+    })
+});
+
+app.get('/api/logout',auth, (req,res) => {
+    req.user.deleteToken(req.token, (err,user)=> {
+        if (err) return res.status(400).send(err)
+        res.sendStatus(200)
+    })
+})
+
 app.get ('/api/getBook', (req,res) => {
     let id = req.query.id;
 
@@ -36,6 +56,34 @@ app.get ('/api/books', (req,res) => {
     })
 })
 
+app.get('/api/getReviewer',(req,res)=> {
+    let id = req.query.id;
+
+    User.findById(id, (err,doc)=> {
+        if (err) return res.status(400).send(err);
+
+        res.json({
+            name: doc.name,
+            lastname: doc.lastname
+        })
+    })
+})
+
+app.get('/api/users',(req,res)=>{
+    User.find({},(err,users)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(users)
+    })
+})
+
+app.get ('/api/user_posts',(req,res) => {
+    Book.find({ownerId:req.query.user}).exec((err,doc) => {
+        if (err) return res.status(400).send(err)
+
+        res.send(doc)
+    })
+})
+
 //POST
 app.post ('/api/book', (req,res) => {
     const book = new Book (req.body)
@@ -45,6 +93,47 @@ app.post ('/api/book', (req,res) => {
         res.status (200).json({
             post: true,
             bookID: doc._id
+        })
+    })
+})
+
+app.post ('/api/register', (req,res) => {
+    const user = new User (req.body);
+
+    user.save((err,doc) => {
+        if (err) {
+            return res.json({success:false});
+        }
+        res.status(200).json({
+            success:true,
+            user:doc
+        })
+    })
+})
+
+app.post ('/api/login', (req,res) => {
+    User.findOne ({'email': req.body.email}, (err, user) => {
+        if (!user) return res.json({
+            isAuth:false,
+            message: 'Authentication failed, User not found'
+        })
+
+        user.comparePassword (req.body.password, (err, isMatch) => {
+            if (!isMatch) return res.json({
+                isAuth:false,
+                message: 'Wrong password'
+            })
+            
+            user.generateToken ((err,user) => {
+                if (err) return res.status(400).send(err)
+
+                res.cookie('auth', user.token,{httpOnly:false}).json({
+                    isAuth:true,
+                    id:user._id,
+                    email: user.email
+                })
+            })
+
         })
     })
 })
