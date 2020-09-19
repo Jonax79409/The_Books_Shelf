43a5707912b92fb47d1b09547f5717daa5d1ebfc
Gaurav <gourav228@gmail.com>
2020-09-19 15:39:02 +0530

    Updated initial posts counts and loadmore post counts

--- client/src/containers/home_container.js
+++ client/src/containers/home_container.js
@@ -6,7 +6,7 @@ import BookItem from '../widgetsUI/book_item'
 class HomeContainer extends Component {
 
     componentWillMount() {
-        this.props.dispatch(getBooks(1,0,'desc'))
+        this.props.dispatch(getBooks(10,0,'desc'))
     }
 
     renderItems = (books) => (
@@ -18,7 +18,7 @@ class HomeContainer extends Component {
 
     loadmore = () => {
         let count = this.props.books.list.length
-        this.props.dispatch(getBooks(1,count,'desc',this.props.books.list))
+        this.props.dispatch(getBooks(5,count,'asc',this.props.books.list))
     }
 
     render() {
